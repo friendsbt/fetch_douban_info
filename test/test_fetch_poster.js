b6d5a194ed57a8acc2f0d42010e7ec3e31a16eb3
laike9m <laike9m@gmail.com>
2014-10-29 14:13:51 +0800

    fix a bug

---
+++ test/test_fetch_poster.js
@@ -0,0 +1,35 @@
+var fs = require('fs');
+var path = require('path');
+var bufferEqual = require('buffer-equal');
+var douban = require('../douban');
+
+var correct_bytes = fs.readFileSync(path.join(__dirname, "poster_nanjing.jpg"));
+
+var s = new douban.Searcher("南京南京");
+s.search(function(info){
+    if (!info) {
+        console.log("fail");
+    } else {
+        var f1 = new douban.posterFetcher(info.ilink);
+        var f2 = new douban.posterFetcher(info.mlink);
+        var f3 = new douban.posterFetcher(info.id);
+        f1.fetch(function(data){
+            if (bufferEqual(data, correct_bytes))
+                console.log("ilink test pass");
+            else
+                console.log("ilink test fail");
+        });
+        f2.fetch(function(data){
+            if (bufferEqual(data, correct_bytes))
+                console.log("mlink test pass");
+            else
+                console.log("mlink test fail");
+        });
+        f3.fetch(function(data){
+            if (bufferEqual(data, correct_bytes))
+                console.log("id test pass");
+            else
+                console.log("id test fail");
+        });
+    }
+});
